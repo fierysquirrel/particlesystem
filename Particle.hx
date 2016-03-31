d6f4b0df88deadf8a8115801b7a46d70900c896a
Henry Fernández <henry@fierysquirrel.com>
2016-03-31 18:56:00 +0900

    Initial commit

---
+++ Particle.hx
@@ -0,0 +1,112 @@
+package;
+
+import aze.display.TileLayer;
+import aze.display.TileSprite;
+import com.fs.fluffeaters.Color;
+import flash.geom.Point;
+
+/**
+ * ...
+ * @author Henry D. Fernández B.
+ */
+class Particle extends TileSprite
+{
+	var velocity : Point;
+	var acceleration : Point;
+	var timer : Float;
+	var maxTime : Float;
+	var isDead : Bool;
+	var partScale : Float;
+	var partAlpha : Float;
+	var partRotation : Float;
+	var initialScale : Float;
+	var initialAlpha : Float;
+	var disappear : Bool;
+	var action : String;
+	
+	public function new(tileLayer : TileLayer,name : String,pos : Point, vel : Point, acc : Point, scale : Float, rot : Float, alpha : Float, time : Float,  color : Color, disappear : Bool = true, action : String = "all") 
+	{
+		super(tileLayer,name);
+		
+		this.maxTime = time;
+		this.velocity = GraphicManager.FixPoint2Screen(vel);
+		this.acceleration = GraphicManager.FixPoint2Screen(acc);
+		this.disappear = disappear;
+		this.action = action;
+		x = pos.x;
+		y = pos.y;
+		initialScale = scale * GraphicManager.GetFixScale();
+		initialAlpha = alpha;
+		partScale = scale * GraphicManager.GetFixScale();
+		partAlpha = alpha;
+		partRotation = rot;
+		r = color.r;
+		g = color.g;
+		b = color.b;
+		this.alpha = partAlpha;
+		this.scale = partScale;
+		this.rotation = partRotation;
+		timer = 0;
+		
+	}
+	
+	public function Update(gameTime:Float):Void 
+	{
+		UpdateVelocity();
+		UpdatePosition();
+
+		if (timer >= MathHelper.ConvertSecToMillisec(maxTime))
+		{
+			isDead = true;
+		}
+		else
+		{
+			if (action == "all")
+			{
+				UpdateAlpha();
+				UpdateScale();
+				UpdateRotation();
+			}
+			
+			timer += gameTime;
+		}
+		
+		if(disappear)
+			alpha = partAlpha;
+		else
+			alpha = 1;
+			
+		scale = partScale;
+		rotation = partRotation;
+	}
+	
+	private function UpdateVelocity() : Void
+	{
+		velocity.x += acceleration.x;
+		velocity.y += acceleration.y;
+	}
+	
+	private function UpdatePosition() : Void
+	{
+		x += velocity.x;
+		y += velocity.y;
+	}
+	
+	private function UpdateRotation() : Void
+	{}
+	
+	private function UpdateAlpha() : Void
+	{
+		partAlpha = initialAlpha * (1 - (timer/MathHelper.ConvertSecToMillisec(maxTime)));
+	}
+	
+	private function UpdateScale() : Void
+	{
+		partScale = initialScale * (1 - (timer/MathHelper.ConvertSecToMillisec(maxTime)));
+	}
+	
+	public function IsDead() : Bool
+	{
+		return isDead;
+	}
+}
---
+++ ParticleSystem.hx
@@ -0,0 +1,133 @@
+package;
+
+import aze.display.TileLayer;
+import aze.display.TileSprite;
+import flash.geom.Point;
+import com.fs.fluffeaters.Color;
+import com.fs.fluffeaters.events.ParticlesEvent;
+
+/**
+ * ...
+ * @author Henry D. Fernández B.
+ */
+class ParticleSystem
+{
+	static public var NUMBER_OF_PARTICLES : Int = 100;
+	
+	private var layers : Map<String,Layer>;
+	private var particles : Array<Particle>;
+	
+	public function new() 
+	{
+		particles = new Array<Particle>();
+	}
+	
+	public function LoadContent(layers : Map<String,Layer>) : Void
+	{
+		this.layers = layers;
+	}
+	
+	public function Update(gameTime:Float):Void 
+	{
+		for (p in particles)
+		{
+			p.Update(gameTime);
+			if (p.IsDead())
+			{
+				particles.remove(p);
+				p.parent.removeChild(p);
+			}
+		}
+	}
+	
+	public function GenerateParticles(layer : String, names : Array<String>,number : Int,pos : Point,size : Float,direction : String,speed : Point,acceleration : Point, colors : Array<Color>,time : Float = 0, rotation : Float = 0, disappear : Bool = true, action : String = "all") : Void
+    {
+		var part : Particle;
+		var c : Color;
+		var vel : Point;
+		var idx : Int;
+		var rot : Float;
+		var name :String;
+			
+		if (rotation != 0)
+			rot = rotation;
+		else
+			rot = 0;
+			
+		for (i in 0...number)
+		{	
+			idx = Math.round(Math.random() * (colors.length -1));
+			c = colors[idx];
+			
+			idx = Math.round(Math.random() * (names.length -1));
+			name = names[idx];
+			
+			switch(direction)
+			{
+				case "up":
+					vel = new Point(Math.sin(2 * Math.random() - 1) * 2, Math.sin( -Math.random()));
+					pos = new Point(pos.x + Math.sin(2 * Math.random() - 1), pos.y + Math.cos(2 * Math.random() - 1));
+				case "side":
+					vel = new Point(Math.random(), 2 * Math.random() - 1);
+				case "random":
+					vel = new Point(2 * Math.random() - 1, 2 * Math.random() - 1);
+				case "radial":
+					vel = new Point(1, 1);
+				case "sin":
+					vel = new Point(Math.random(),Math.random());
+				default:
+					vel = new Point();
+					
+			}
+			
+			vel.x *= speed.x;
+			vel.y *= speed.y;
+			
+			part = new Particle(layers.get(layer),name,pos,vel,acceleration, size,rotation,0.1 + Math.random(),0.1 + (time * Math.random()),c,disappear,action);
+			particles.push(part);
+			layers.get(layer).addChild(part);
+		}
+    }
+	
+	public function HandleParticlesEvent(e : ParticlesEvent) : Void
+	{
+		var names : Array<String>;
+		var layer,direction : String;
+		var number : Int;
+		var pos, vel,acc : Point;
+		var colors : Array<Color>;
+		var size, time, radius, gravity : Float;
+		var disappear : Bool;
+		var action : String;
+		
+		layer = e.GetLayer();
+		names = e.GetNames();
+		direction = e.GetDirection();
+		number = e.GetNumber();
+		pos = e.GetPos();
+		vel = e.GetVel();
+		acc = e.GetAcc();
+		colors = e.GetColors();
+		size = e.GetSize();
+		time = e.GetTime();
+		disappear = e.GetDisappear();
+		action = e.GetAction();
+		
+		GenerateParticles(layer, names, number, pos, size, direction, vel,acc, colors,time,0,disappear,action);
+	}
+	
+	public function Clean() : Void
+	{
+		for (p in particles)
+		{
+			if(p.parent != null)
+				p.parent.removeChild(p);
+			particles.remove(p);
+		}
+	}
+	
+	public function GetParticles() : Array<Particle>
+	{
+		return particles;
+	}
+}
